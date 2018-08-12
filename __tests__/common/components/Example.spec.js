bf283c33ef72ed60c509136759749620548b3105
Sm3llin <michaelellin@gmail.com>
2018-08-12 17:53:11 +1000

    Added tests to cover the MOTD updating the title correctly

--- __tests__/common/components/Example.spec.js
+++ __tests__/common/components/Example.spec.js
@@ -12,6 +12,15 @@ const fixture = {
   },
 };
 
+const motdFixture = {
+  example: {
+    result: fromJS({
+      testing: 'data',
+    }),
+    motd: "Hello World!"
+  },
+};
+
 describe('ExampleView', () => {
   it('should render a blank div without data', () => {
     const el = shallow(<Example />)
@@ -27,5 +36,22 @@ describe('ExampleView', () => {
 
     expect(el.length).toEqual(1)
     expect(el.find('.exampleOutput').length).toEqual(1)
+    expect(el.find('h1').find('span').text()).toEqual('Started')
+  })
+
+  it('should render a default motd message when none is present', () => {
+    const el = shallow(
+      <Example {...fixture} />
+    )
+
+    expect(el.find('h1').find('span').text()).toEqual('Started')
+  })
+
+  it('should render the motd if store contains a string', () => {
+    const el = shallow(
+      <Example {...motdFixture} />
+    )
+
+    expect(el.find('h1').find('span').text()).toEqual(motdFixture.example.motd)
   })
 })
