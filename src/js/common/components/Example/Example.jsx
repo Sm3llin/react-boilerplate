cb99aed60e0456b26a50c43cd3d60480f1715f52
Sm3llin <michaelellin@gmail.com>
2018-08-12 17:29:15 +1000

    Added a input example of redux-persist on the ExamplePage

--- src/js/common/components/Example/Example.jsx
+++ src/js/common/components/Example/Example.jsx
@@ -6,12 +6,19 @@ class Example extends PureComponent {
   render() {
     const { example } = this.props;
     const result = example && example.result ? example.result : null;
+    const motd = example && example.motd ? example.motd : null;
 
     if (result && result.size && result.size > 0) {
       return (
         <div className="exampleOutput">
-          <h1>Let&apos;s Get <span className="emphasize">Started</span></h1>
+          <h1>Let&apos;s Get <span className="emphasize">{ motd || 'Started'}</span></h1>
           <p>If you see this screen, it means you are all setup \o/</p>
+
+          <i>Example of persistent storage: </i>
+          <input
+            value={this.props.example.motd}
+            onChange={({ target }) => this.props.updateMotd(target.value)}
+          />
           <p>The following JSON are showing contents coming from Redux, Saga and Config.</p>
           <pre>
             {JSON.stringify(result.toJS(), undefined, 2)}
--- src/js/redux/modules/example.js
+++ src/js/redux/modules/example.js
@@ -6,10 +6,12 @@ import type { exampleType } from '../../common/types/example'
 
 const GET_EXAMPLE = 'app/example/GET_EXAMPLE';
 const UPDATE_EXAMPLE = 'app/example/UPDATE_EXAMPLE';
+const UPDATE_MOTD = 'app/example/UPDATE_MOTD';
 
 export const constants = {
   GET_EXAMPLE,
   UPDATE_EXAMPLE,
+  UPDATE_MOTD,
 };
 
 // ------------------------------------
@@ -17,10 +19,12 @@ export const constants = {
 // ------------------------------------
 export const getAwesomeCode = createAction(GET_EXAMPLE, () => ({}));
 export const updateExample = createAction(UPDATE_EXAMPLE, (result : exampleType) => ({ result }));
+export const updateMotd = createAction(UPDATE_MOTD, (value) => ({ value }));
 
 export const actions = {
   getAwesomeCode,
   updateExample,
+  updateMotd,
 };
 
 export const reducers = {
@@ -31,11 +35,13 @@ export const reducers = {
     state.merge({
       ...payload,
     }),
-}
+  [UPDATE_MOTD]: (state, { payload }) => state.merge({ motd: payload.value }),
+};
 
 export const initialState = () =>
   Map({
     result: '',
+    motd: '',
   })
 
 export default handleActions(reducers, initialState());
